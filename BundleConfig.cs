42137344755dbf8ada9d1b087e6a209a104fd093
rhale1 <48366920+rhale1@users.noreply.github.com>
2019-06-09 17:23:14 -0700

    Add files via upload

---
+++ BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ScheduleIt2._0
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+            
+            //Modified to bootstrap bundle from bootstrap to include popper for modal bootstrap dropdown menu (kw)
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.bundle.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      //jQuery fullcalendar plugin css
+                      "~/Content/fullcalendar.css"));
+            
+            //jQuery fullcalendar plugin js
+            bundles.Add(new ScriptBundle("~/bundles/fullcalendar").Include(
+                      "~/Scripts/moment.js",
+                       "~/Scripts/fullcalendar.js",
+                       "~/Scripts/fullcalendar-custom.js"));
+        }
+    }
+}
