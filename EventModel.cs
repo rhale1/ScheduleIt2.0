7938bbdc6b2a1ce4e64a52fa40123230cdaa18e5
rhale1 <48366920+rhale1@users.noreply.github.com>
2019-06-09 17:05:44 -0700

    Add files via upload

---
+++ EventModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ScheduleIt2._0.Models
+{
+    public class EventModel
+    {
+        [Key]
+        /// <summary>
+        ///event id property
+        /// </summary>
+        public Guid EventId { get; set; }
+
+        /// <summary>
+        ///start time of event property
+        /// </summary>
+        public DateTime? StartTime { get; set; }
+
+        /// <summary>
+        ///end time of event property
+        /// </summary>
+        public DateTime? EndTime { get; set; }
+
+        /// <summary>
+        ///note attached to event property
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        ///title of event property
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        ///id of admin who approved event property
+        /// <summary>
+        public int AdminId { get; set; }
+ 
+        /// <summary>
+        ///conntects to attached user
+        /// </summary>
+        public virtual ApplicationUser User { get; set; }
+    }
+}
+
+    
+    
---
+++ WorkTimeEventModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScheduleIt2._0.Models
+{
+    public class WorkTimeEventModel : EventModel
+    {
+        /// <summary>
+        /// Model for when a user clocks in or out, stored in the Events Table
+        /// </summary>
+        /// 
+        public WorkTimeEventModel()
+        {
+        }
+        //Clocks the user in by creating a new worktimeevent
+        public WorkTimeEventModel(ApplicationUser user, string message, DateTime? dt = null)
+        {
+            User = user;
+            EventId = Guid.NewGuid();
+            if (dt.HasValue) StartTime = dt.Value;
+            else StartTime = DateTime.Now;
+            Message = message;
+        }
+    }
+}
